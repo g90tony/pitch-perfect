69a023c3609f80403bb755bc1188792b5448d754
Caleb Tony <calebmbugua@gmail.com>
2021-04-27 00:55:23 +0300

    added config classes and imported necessary keys

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+class Config:
+    # WTF FORMS SECURITY KEY
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    
+    # SQLALCHEMY KEYS
+    SQLALCHEMY_DATABASE_URI =  'postgresql+psycopg2://caleb:admin@localhost/pitch_perfect'
+    SQLALCHEMY_TRACK_NOTIFICATIONS = True
+    
+    # DISPLAY PICTURE UPLOAD DESTINATION
+    UPLOADED_PHOTOS_DEST = 'app/static/images'
+    
+    # MAIL CONFIGURATION
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    
+    DEBUG = True
+    
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
