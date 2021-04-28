0e6ddddbd10c622b4750a26209298bb48de8e5c5
Caleb Tony <calebmbugua@gmail.com>
2021-04-29 02:27:24 +0300

    added form submit handler

--- app/main/views.py
+++ app/main/views.py
@@ -5,9 +5,19 @@ from .. import db
 from ..models import Category, Pitch
 from flask_login import login_required, current_user
 
-@main.route('/')
+@main.route('/', methods=['POST', 'GET'])
 def index():
     title = 'Pitch Perfect: Perfect your pitches'
+    new_pitch = PitchForm()
+    
+    if new_pitch.validate_on_submit():
+        category_id = Category.query.filter_by(title = new_pitch.category).first()
+        pitch = Pitch(body=new_pitch.new_pitch, category_id = category_id, user_id = current_user.id, up_votes = 0, down_votes=0)
+        
+        db.session.add(pitch)
+        db.session.commit()
+        
+        return redirect( url_for('main.index'))
     
     pitches = Pitch.get_pitches()
     if current_user.is_authenticated:
@@ -15,4 +25,4 @@ def index():
     else:
         authenticated = False
     
-    return render_template('index.html', title=title, pitches= pitches, authenticated=authenticated)
+    return render_template('index.html', title=title, pitches= pitches, authenticated=authenticated, form = new_pitch)
