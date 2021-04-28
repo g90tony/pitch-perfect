94b0b27258afe146067be4b3234868ed4c5f766b
Caleb Tony <calebmbugua@gmail.com>
2021-04-28 11:31:08 +0300

    added login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,3 +24,19 @@ def login():
     
     return render_template('auth/login', login_form = new_login, title = title)
 
+@auth.route('/register')
+def register():
+    new_user = RegistrationForm()
+    
+    if new_user.validate_on_submit():
+        user = User(id,first_name = new_user.first_name.data,last_name = new_user.last_name.data ,user_email = new_user.email.data ,bio = '' ,avatar = new_user.avatar ,hash_pass = new_user.password) 
+        db.sessions.add(user)
+        db.session.commit()
+        
+        welcome_email('Welcome to Pitch Perfect', 'email/welcome_user', user.email, user= user)
+        
+        return redirect(url_for('auth.login'))
+    
+    title = 'Create an account: Pitch Perfect Sign-up'     
+    
+    return render_template('auth/register.html', registration_form = new_user, title= title)
