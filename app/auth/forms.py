1d0b3e96df243c55388c6aeed9fae98320b35b99
Caleb Tony <calebmbugua@gmail.com>
2021-04-28 18:59:58 +0300

    changed email field to string field and removed uneccessary white spaces

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, PasswordFields, SubmitField, FileField, BooleanField, ValidationErrors
+from wtforms import StringField, PasswordField, SubmitField, FileField, BooleanField, ValidationError
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
 
 class RegistrationForm(FlaskForm):
     first_name = StringField('Enter first name', validators = [(Required())])
     last_name = StringField('Enter last name', validators = [Required()])
-    email = EmailField('Enter email address', validators = [(Required(), Email())])
+    email = StringField('Enter email address', validators = [Required(), Email()])
     avatar = FileField('Upload Profile Picture', validators = [Required()])
-    password = PasswordFields('Enter new password', validators = [Required()])
-    password2 = PasswordFields('Confrim new password', validators = [Required()])
+    password = PasswordField('Enter new password', validators = [Required()])
+    password2 = PasswordField('Confrim new password', validators = [Required()])
     submit = SubmitField('Create account')
 
 class LoginForm(FlaskForm):
-    email = EmailField('Enter email address', validators = [(Required(), Email())])
-    password = PasswordFields('Enter your password', validators = [Required()])
-    remember_user =     BooleanField('Remember me')
+    email = StringField('Enter email address', validators = [Required(), Email()])
+    password = PasswordField('Enter your password', validators = [Required()])
+    remember_user = BooleanField('Remember me')
     submit = SubmitField('Login')
