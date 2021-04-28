8b266624909d442dd16029b8c1efe686fbd67744
Caleb Tony <calebmbugua@gmail.com>
2021-04-28 11:29:52 +0300

    added registration form

---
+++ app/auth/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordFields, SubmitField, FileField, BooleanField, ValidationErrors
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('Enter first name', validators = [(Required())])
+    last_name = StringField('Enter last name', validators = [Required()])
+    email = EmailField('Enter email address', validators = [(Required(), Email())])
+    avatar = FileField('Upload Profile Picture', validators = [Required()])
+    password = PasswordFields('Enter new password', validators = [Required()])
+    password2 = PasswordFields('Confrim new password', validators = [Required()])
+    submit = SubmitField('Create account')
+
