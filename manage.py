17e595ddeb0ccde0ca8ff5e714580c0a919b9234
Caleb Tony <calebmbugua@gmail.com>
2021-04-28 19:06:24 +0300

    added models to shell context
    fixed typos in line 7, line 17 and line 18

--- manage.py
+++ manage.py
@@ -1,18 +1,20 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
+from app.models import Category, Comment, Pitch, User
 
 app = create_app('development')
+
 manager = Manager(app)
-migrate = MigrateCommand(app, db)
+migrate = Migrate(app, db)
 
 
 manager.add_command('db', MigrateCommand)
 manager.add_command('server', Server)
 
 @manager.shell
-def make_shell_context:
-    return dict(app = app, db = db, User = User)
+def make_shell_context():
+    return dict(app = app, db = db, Category = Category, Comment = Comment, Pitch = Pitch, User = User)
 
-if __name__ = '__main__':
-    manager.run
+if __name__ == '__main__':
+    manager.run()
