430025327dfe6bea97c1e09e975312d1f5ceb6be
Caleb Tony <calebmbugua@gmail.com>
2021-04-29 02:26:25 +0300

    removed unneccessary 'app_'

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import main
 
-@main.app_errorhandler(404)
+@main.errorhandler(404)
 def not_found_error(error):
     
     return render_template('404.html'), 404
